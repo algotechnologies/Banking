95c8b10175183d152c6ff5d627815b63ff218dcd
Saad Aftab <saadaftab1990@gmail.com>
2018-05-08 13:23:03 +0500

    Minor changes

--- app/src/main/java/com/wow/app/banking/utilities/Constants.java
+++ app/src/main/java/com/wow/app/banking/utilities/Constants.java
@@ -3,7 +3,6 @@ package com.wow.app.banking.utilities;
 public class Constants {
 
     public static final String TAG = "BANKING";
-
-    public static double INTEREST_RATE = 2.5;
+    public static final double INTEREST_RATE = 2.5;
 
 }
